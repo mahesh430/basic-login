4d897eb03b53fc71f99272a2f45ce2bffbc45b5e
Bharath Pantala <bpantala@securonix.com>
2022-08-19 08:48:44 +0530

    1.0

---
+++ src/test/java/com/cloudrural/basiclogin/BasicLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudrural.basiclogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
